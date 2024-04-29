7c2dd907f0a5fd8830b9c0bd5a314cb9787fc796
hfator <93863553+hfator@users.noreply.github.com>
2024-04-29 12:58:23 +0200

    Update index.js to include database

--- index.js
+++ index.js
@@ -3,10 +3,22 @@ const express = require('express');
 const cors = require('cors');
 const dns = require('dns');
 const url = require('url');
+const mongoose = require('mongoose');
+const { count } = require('console');
 const app = express();
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
+const mongoDb = process.env.MONDB;
+
+mongoose.connect(mongoDb, { useNewUrlParser: true, useUnifiedTopology: true});
+
+const urlSchema = new mongoose.Schema({
+  original_url: String,
+  short_url: Number
+})
+
+const Url = mongoose.model('Url', urlSchema)
 
 app.use(cors());
 app.use(express.json());
@@ -22,37 +34,45 @@ app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
 
-let urlDatabase = {};
-let id = 0;
-
 app.post('/api/shorturl', function (req, res) {
   const originalUrl = req.body.url;
   const parsedUrl = url.parse(originalUrl);
 
-  if (!/^https?:\/\//.test(originalUrl)) {
-    res.json({ error: 'invalid URL' });
-    return;
-  }
+  const validUrlRegex = /^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/i;
 
+  if (!validUrlRegex.test(originalUrl)) {
+    return res.json({ error: 'invalid URL' });
+  }
+  
   dns.lookup(parsedUrl.hostname, (err) => {
     if (err) {
       res.json({ error: 'invalid URL' });
     } else {
-      id++;
-      urlDatabase[id] = originalUrl;
-      res.json({ original_url: originalUrl, short_url: id });
+      Url.findOne({ original_url: originalUrl }).then(foundUrl => {
+        if (foundUrl) {
+          res.json({ original_url: foundUrl.original_url, short_url: foundUrl.short_url });
+        } else {
+          Url.estimatedDocumentCount().then(count => {
+            let newUrl = new Url({ original_url: originalUrl, short_url: count + 1 });
+            newUrl.save().then(savedUrl => {
+              res.json({ original_url: savedUrl.original_url, short_url: savedUrl.short_url });
+            });
+          });
+        }
+      });
     }
   });
 });
 
-app.get('/api/shorturl/:short_url', function (req, res) {
+app.get('/api/shorturl/:short_url', function(req, res) {
   const shortUrl = req.params.short_url;
-  const originalUrl = urlDatabase[shortUrl];
-  if (originalUrl) {
-    res.redirect(originalUrl);
-  } else {
-    res.json({ error: 'No short URL found' });
-  }
+  Url.findOne({ short_url: shortUrl }).then(foundUrl => {
+    if (foundUrl) {
+      res.redirect(foundUrl.original_url);
+    } else {
+      res.json({ error: 'No short URL found' });
+    }
+  });
 });
 
 app.listen(port, function() {
